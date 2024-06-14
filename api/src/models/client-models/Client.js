f115c37d4441672864f1a01347f03f0867d1daf3
Elevate Soccer Performance <tim.williams@elevatesoccerperformance.com>
2024-06-13 18:06:25 -0600

    client model

--- api/src/models/client-models/Client.js
+++ api/src/models/client-models/Client.js
@@ -1,17 +1,53 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const ClientSchema = new Schema({
-  athlete_name: {
-    type: String,
+const ClientSchema = new Schema(
+  {
+    athlete_name: {
+      type: String,
+    },
+    club_level: {
+      type: String,
+      enum: ["EAL", "ECRL", "ECNL", "MLS-Next", "Other"],
+    },
+    athlete_year: {
+      type: String,
+    },
+    parent_name: {
+      type: String,
+    },
+    phone: {
+      unique: true,
+      type: String,
+      required: [true, "Please add your phone number"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: "Email address is required",
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    status: {
+      type: String,
+      enum: ["Active", "In-Active"],
+    },
+    athlete_dob: {
+      type: Date,
+    },
+    assigned_user: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
   },
-  parent_name: {
-    type: String,
-  },
-  phone: {},
-  email: {},
-  athlete_dob: {},
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Client = mongoose.model("clients", ClientSchema);
 
