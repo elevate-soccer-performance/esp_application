631d5a2bdad37c25e0e4e93847fcc38f8c1769f9
Elevate Soccer Performance <tim.williams@elevatesoccerperformance.com>
2024-09-24 16:22:11 -0600

    save

--- api/src/models/metric-models/FMS.js
+++ api/src/models/metric-models/FMS.js
@@ -5,6 +5,45 @@ const FMSSchema = new Schema({
   date: {
     type: Date,
   },
+  impigment_clearing_test_result: {
+    type: Boolean,
+  },
+  press_up_clearing_test_result: {
+    type: Boolean,
+  },
+  posterior_rocking_clearing_test_result: {
+    type: Boolean,
+  },
+  deep_squat_result: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
+  hurdle_step_result: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
+  inline_lunge_result: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
+  active_straight_leg_raise_result: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
+  trunk_stability_pushup_result: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
+  rotary_statbility_result: {
+    type: Number,
+    min: 0,
+    max: 3,
+  },
 });
 
 const FMS = mongoose.model("fms", FMSSchema);
